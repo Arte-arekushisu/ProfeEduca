6e9d895d8318c8bdeba96c3d3644ddb520d346fb
Arte-arekushisu <profeeduca19@gmail.com>
2026-02-23 19:29:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -29,15 +29,4 @@ if 'db' not in st.session_state:
 
 # --- FLUJO DE PANTALLAS ---
 if st.session_state.db["step"] == "registro_email":
-    col1, col2, col3 = st.columns([1, 1.5, 1])
-    with col2:
-        st.markdown('<div style="text-align:center; font-size:4rem;">💼</div>', unsafe_allow_html=True)
-        st.title("🍎 ProfeEduca")
-        st.subheader("Planeaciones para el Maestro ABCD")
-        st.write("Inicia tu registro empresarial ingresando tu correo.")
-        email = st.text_input("Correo Electrónico")
-        if st.button("Enviar Código de Seguridad"):
-            if "@" in email:
-                st.session_state.db["temp"]["email"] = email
-                st.session_state.db["temp"]["code"] = str(random.randint(100000, 999999))
-                st.session_state.db["step"] = "
+    col1,
