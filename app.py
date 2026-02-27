b518b62564b0b45ec6bb57ac60b935a84e1bf195
Arte-arekushisu <profeeduca19@gmail.com>
2026-02-26 22:27:00 -0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="PROFEEDUCA MASTER", layout="wide", page_icon="🎓")
+
+st.title("🎓 Sistema Integral PROFEEDUCA")
+st.write("### ¡Hola Axel! Selecciona una fase en el menú de la izquierda para comenzar.")
+
+st.info("👈 Si no ves el menú, dale clic a la flechita arriba a la izquierda.")
